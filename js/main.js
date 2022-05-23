f716e59f0fec59d0cb4f3cbf0234446532dc833e
TrvMarco <trevisanello.marco@gmail.com>
2022-05-23 17:39:24 +0200

    aggiunto interazione con nuova attività

--- js/main.js
+++ js/main.js
@@ -26,21 +26,33 @@ const app = new Vue({
                 text: 'Ordinare orologio',
                 isDone: false,
             }
-        ]    
+        ],  
+        newTodo: {
+            text: '',
+            isDone: false
+        }
     },
     methods:{
         checkActivity(index){
             if(!this.todolist[index].isDone){
                 this.todolist[index].isDone = true;
-                
+
             }else{
                 this.todolist[index].isDone = false;
             }    
         },
         cancelActivity(index){
             this.todolist.splice(index,1);
+        },
+        addTask(){
+            if(!this.todolist.includes(this.newTodo)){
+                this.todolist.push(this.newTodo);
+                this.newTodo = {
+                    text: '',
+                    isDone: false}
+            }
         }
-    }
+    }   
      
     
 })
